de93a2c1547fe7050a7bf8a9476aacd75c4db984
Vakisr <vakisr@gmail.com>
2026-06-04 00:10:05 +0200

    Add onRequestGet function to fetch state data

---
+++ functions/api/state.js
@@ -0,0 +1,15 @@
+export async function onRequestGet({ env }) {
+  const [votesRaw, coachesRaw, contribRaw] = await Promise.all([
+    env.VOTES.get("votes"),
+    env.VOTES.get("coaches"),
+    env.VOTES.get("contributors"),
+  ]);
+
+  return Response.json({
+    votes:        votesRaw    ? JSON.parse(votesRaw)   : {},
+    coaches:      coachesRaw  ? JSON.parse(coachesRaw) : { slot: 9120, glasner: 11890 },
+    contributors: contribRaw  ? Number(contribRaw)     : 14207,
+  }, {
+    headers: { "Cache-Control": "no-store" },
+  });
+}
